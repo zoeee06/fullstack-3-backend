428ffb0f2b80e29161e852b2c590d68ced2818dc
Zoe Liu <107218898+zoeee06@users.noreply.github.com>
2025-07-03 23:05:26 +0800

    add get user by id

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import { listUsers, createUser } from "../controller/users";
+import { listUsers, createUser, getUser } from "../controller/users";
 
 const router = Router();
 
 router.get("/users", listUsers);
 router.post("/users", createUser);
-
+router.get("/users/:id", getUser);
 export default router;
